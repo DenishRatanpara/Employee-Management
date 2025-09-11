4c7a212fc59d4f457a3b8b12f5625f5c710cf86b
DenishRatanpara <denishratanpara56@gmail.com>
2025-09-11 10:55:50 +0530

    some changes in server

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,6 @@ import dotenv from "dotenv";
 import cors from "cors";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
-import path from "path";
-import { fileURLToPath } from "url";
 
 // Custom imports
 import connectToDatabase from "./db/db.js";
@@ -24,31 +22,28 @@ userRegister();
 
 const app = express();
 
-// Fix __dirname for ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 // Middleware
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: [
-      process.env.CORS_ORIGIN, // local dev
-      "https://employee-management-frontend-ekvj.onrender.com", // deployed frontend
-    ],
-    credentials: true,
-  })
-);
+
+
+
+app.use(cors({
+  origin: [
+    process.env.CORS_ORIGIN, // for local dev
+    "https://employee-management-frontend-ekvj.onrender.com",
+  // deployed frontend
+  ],
+  credentials: true, // if you are sending cookies or authorization headers
+}));
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public/upload"));
-
 app.get("/test", (req, res) => {
   res.json({ message: "CORS is working!" });
 });
-
-// API Routes
+// Routes
 app.use("/users", LoginRoute);
 app.use("/department", addDepartment);
 app.use("/employee", empRouter);
@@ -56,11 +51,5 @@ app.use("/salary", salaryRouter);
 app.use("/leave", leaveRouter);
 app.use("/settings", setting);
 
-// Serve React frontend (after APIs)
-app.use(express.static(path.join(__dirname, "client/build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
 
 export default app;
